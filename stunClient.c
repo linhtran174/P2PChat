1ca42777e7667341010e5dfd11c9d72422035d20
Linh Tran <linhtran17495@gmail.com>
2017-04-10 11:31:19 +0700

    demo stunClient complete

--- stunClient.c
+++ stunClient.c
@@ -5,9 +5,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+int error(char *message);
 
 //Provide STUN Client API:
-int stun_xor_addr(int localSoc, char * return_ip, unsigned short * return_port)
+int getPublicIpAddress(char * returnIp, unsigned short * returnPort)
 {
 	//create local socket with port 12345
 	int localSoc = socket(AF_INET, SOCK_DGRAM, 0);
@@ -15,8 +16,7 @@ int stun_xor_addr(int localSoc, char * return_ip, unsigned short * return_port)
     bzero(&localAddr, sizeof(localAddr));
     localAddr.sin_family = AF_INET;
     localAddr.sin_port = htons(12345);
-	bind(sockfd, (struct sockaddr *)&localAddr, sizeof(localAddr));
-
+	bind(localSoc, (struct sockaddr *)&localAddr, sizeof(localAddr));
 
 	//create server socket
 	struct sockaddr_in serverAddr;
@@ -37,15 +37,11 @@ int stun_xor_addr(int localSoc, char * return_ip, unsigned short * return_port)
 	*(int *)(&STUNRequest[12])= htonl(0x0714278f);
 	*(int *)(&STUNRequest[16])= htonl(0x5ded3221);
 
-
     //send request
     short requestResult =
-    sendto(localSoc, STUNRequest, sizeof(STUNRequest), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr)); // send UDP
-	if (requestResult == -1)
-	{
-		printf("sendto error\n");
-		return -1;
-	}
+    sendto(localSoc, STUNRequest, sizeof(STUNRequest),
+    0, (struct sockaddr *)&serverAddr, sizeof(serverAddr)); // send UDP
+	if (requestResult == -1) return error("cannot send STUN request");
 
 	//wait 100ms for the response
 	usleep(1000 * 100);
@@ -53,35 +49,34 @@ int stun_xor_addr(int localSoc, char * return_ip, unsigned short * return_port)
 	//read response
 	unsigned char buf[300]; //buffer
     requestResult = recvfrom(localSoc, buf, 300, 0, NULL, 0); // recv UDP
-    if (requestResult == -1)
-	{
-	    printf("recvfrom error\n");
-	    return -2;
-	}
+    if (requestResult == -1) return error("cannot receive any response");
 
-	short attr_type;
-	short attr_length;
+	// parse response
 	short STUNSuccess = *(short *)(&buf[0]) == htons(0x0101);
+	short attr_type = htons(*(short *)(&buf[20]));
+	short attr_length = htons(*(short *)(&buf[22]));
 	if (STUNSuccess)
 	{
 		printf("STUN binding resp: success !\n");
-
-		// parse 
-    	attr_type = htons(*(short *)(&buf[i]));
-		attr_length = htons(*(short *)(&buf[i+2]));
 		if (attr_type == 0x0020)
 		{
 			// parse : port, IP 
-			port = (buf[26] * 256 + buf[27]) ^ 0x2112;
-
-			// return 
-			*return_port = port;
-			sprintf(return_ip,"%d.%d.%d.%d",buf[28]^0x21,buf[29]^0x12,buf[30]^0xA4,buf[31]^0x42);
-
-			break;
+			*returnPort = (buf[26] * 256 + buf[27]) ^ 0x2112;
+			sprintf(returnIp,"%d.%d.%d.%d",buf[28]^0x21,buf[29]^0x12,buf[30]^0xA4,buf[31]^0x42);
 		}
 	}
 
-	// do not close the socket !!!
 	return 0;
+}
+
+int error(char *message){
+	printf("ERROR: %s\n", message);
+	return -1;
+}
+
+int main(){
+	char ip[100];
+	unsigned short port = 0;
+	getPublicIpAddress(ip, &port);
+	printf("%s:%d\n", ip, port);
 }
