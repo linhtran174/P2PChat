87d5f0f595655ffaab4b33c17f42b5374b2e3b9e
Linh Tran <linhtt@ows.vn>
2017-05-15 16:28:10 +0700

    replace nameIpMap with name to user map, properly tested

--- dapnatUser.c
+++ dapnatUser.c
@@ -1,13 +1,53 @@
-typedef struct _user{
+#include "socketHelper.c"
+
+struct _user{
+	struct _user *left;
+	struct _user *right;
 	Socket soc;
 	char name[30]; //20 chars
-} *User;
+};
+typedef struct _user *User;
+
+void  mapPut(char *name, User user);
+User mapGet(char *name);
+
+User __root = NULL;
 
 User newUser(char *name, Socket soc){
 	User temp = (User)malloc(sizeof(struct _user));
+	temp->left = NULL;
+	temp->right = NULL;
 	strcpy(temp->name, name);
 	temp->soc = soc;
+
 	return temp;
 }
 
+User getUser(User root, char *name){
+	if(root == NULL) return NULL;
+	int compare = strcmp(root->name, name);
+	if(compare == 0) return root;
+	if(compare >= 0) return getUser(root->left, name);
+	else return getUser(root->right, name);
+}
 
+void putUser(User *root, User new){
+	if(*root == NULL){
+		*root = new;
+		return;
+	}
+	if(strcmp((*root)->name, new->name) >= 0){
+		putUser(&((*root)->left), new );
+	}
+	else{
+		putUser(&((*root)->right), new);
+	}
+}
+
+void mapPut(char *name, User user){
+	putUser(&__root, user);
+}
+
+User mapGet(char *name){
+	return getUser(__root, name);
+}
--- test.c
+++ test.c
@@ -7,15 +7,39 @@
 #include <pthread.h>
 
 // #include "socketHelper.c"
-#include "nameIpMap.c"
+#include "dapnatUser.c"
+void printUser(User user);
 
 int main(){
-	mapPut("Linh Tran", "1.2.3.4");
-	mapPut("Thinh Tran", "12.25.63.56");
-	mapPut("Phung Mai Minh", "123.456.789.10");
-	printf("%s \n", mapGet("Linh Tran"));
-	printf("%s \n", mapGet("Phung Mai Minh"));
-	printf("%s \n", mapGet("Thinh Tran"));
-// 
+
+	mapPut("Linh Tran", 
+		newUser("Linh Tran",
+			newSocket("123.456.789.10", "1234")
+		)
+	);
+
+	mapPut("Thinh Tran",
+		newUser("Thinh Tran",
+			newSocket("12.34.56.78", "2345")
+		)
+	);
+
+	mapPut("Phung Mai Minh",
+		newUser("Phung Mai Minh", 
+			newSocket("34.56.78.910", "3456")
+		)
+	);
+
+	User linh = mapGet("Linh Tran");
+	User phung = mapGet("Phung Mai Minh");
+	User thinh = mapGet("Thinh Tran");
+
+	printUser(linh);
+	printUser(phung);
+	printUser(thinh);
 	pthread_exit(NULL);
+}
+
+void printUser(User user){
+	printf("User: %s at %s:%s\n", user->name, user->soc->ip, user->soc->port);
 }
